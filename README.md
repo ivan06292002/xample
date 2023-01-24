1a1daf0523af062f0c6ca03f2cfe61dfd40f1770
ivan06292002 <100040157+ivan06292002@users.noreply.github.com>
2023-01-24 21:01:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xample
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jam2rn)
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: 'reference', component: ReferenceComponent },
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    AchievementsComponent,
+    ReferenceComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
